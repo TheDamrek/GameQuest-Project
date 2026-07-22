14969d0873f6175d12fd2a3c685c0147f7eec256
TheDamrek <kermadabdenour73@gmail.com>
2026-07-22 02:48:32 +0100

    the hover's left

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -362,7 +362,7 @@ let count = 0;
 
 let chosenWord = "";
 
-//Display option buttons
+
 const displayOptions = () => {
   optionsContainer.innerHTML += `<h3>Please Select An Option</h3>`;
   let buttonCon = document.createElement("div");
@@ -372,120 +372,127 @@ const displayOptions = () => {
   optionsContainer.appendChild(buttonCon);
 };
 
-//Block all the Buttons
+
 const blocker = () => {
   let optionsButtons = document.querySelectorAll(".options");
   let letterButtons = document.querySelectorAll(".letters");
-  //disable all options
+  
   optionsButtons.forEach((button) => {
     button.disabled = true;
   });
 
-  //disable all letters
+  
   letterButtons.forEach((button) => {
-    button.disabled.true;
+    button.disabled = true;
   });
   newGameContainer.classList.remove("hide");
 };
 
-//Word Generator
+
 const generateWord = (optionValue) => {
   let optionsButtons = document.querySelectorAll(".options");
-  //If optionValur matches the button innerText then highlight the button
+  
   optionsButtons.forEach((button) => {
-    if (button.innerText.toLowerCase() === optionValue) {
+    if (button.innerText.toLowerCase() === optionValue.toLowerCase()) {
       button.classList.add("active");
     }
     button.disabled = true;
   });
 
-  //initially hide letters, clear previous word
+  
   letterContainer.classList.remove("hide");
   userInputSection.innerText = "";
 
   let optionArray = options[optionValue];
-  //choose random word
+  
   chosenWord = optionArray[Math.floor(Math.random() * optionArray.length)];
   chosenWord = chosenWord.toUpperCase();
 
-  //replace every letter with span containing dash
+  
   let displayItem = chosenWord.replace(/./g, '<span class="dashes">_</span>');
 
-  //Display each element as span
+  
   userInputSection.innerHTML = displayItem;
+
+
+  let letterButtons = document.querySelectorAll(".letters")
+  letterButtons.forEach((button) => {
+     button.disabled = false;
+  });
 };
 
-//Initial Function (Called when page loads/user presses new game)
+
 const initializer = () => {
   winCount = 0;
   count = 0;
 
-  //Initially erase all content and hide letteres and new game button
+  
   userInputSection.innerHTML = "";
   optionsContainer.innerHTML = "";
   letterContainer.classList.add("hide");
   newGameContainer.classList.add("hide");
   letterContainer.innerHTML = "";
 
-  //For creating letter buttons
+  
   for (let i = 65; i < 91; i++) {
     let button = document.createElement("button");
     button.classList.add("letters");
-    //Number to ASCII[A-Z]
+    
     button.innerText = String.fromCharCode(i);
-    //character button click
+    button.disabled = true;
+    
     button.addEventListener("click", () => {
       let charArray = chosenWord.split("");
       let dashes = document.getElementsByClassName("dashes");
-      //if array contains clciked value replace the matched dash with letter else dram on canvas
+      
       if (charArray.includes(button.innerText)) {
         charArray.forEach((char, index) => {
-          //if character in array is same as clicked button
+         
           if (char === button.innerText) {
-            //replace dash with letter
+          
             dashes[index].innerText = char;
-            //increment counter
+           
             winCount += 1;
-            //if winCount equals word lenfth
+           
             if (winCount == charArray.length) {
               resultText.innerHTML = `<h2 class='win-msg'>You Win!!</h2><p>The word was <span>${chosenWord}</span></p>`;
-              //block all buttons
+              
               blocker();
             }
           }
         });
       } else {
-        //lose count
+        
         count += 1;
-        //for drawing man
+        
         drawMan(count);
-        //Count==6 because head,body,left arm, right arm,left leg,right leg
-        if (count == 6) {
+        
+        if (count === 6) {
           resultText.innerHTML = `<h2 class='lose-msg'>You Lose!!</h2><p>The word was <span>${chosenWord}</span></p>`;
           blocker();
         }
       }
-      //disable clicked button
+     
       button.disabled = true;
     });
     letterContainer.append(button);
   }
 
   displayOptions();
-  //Call to canvasCreator (for clearing previous canvas and creating initial canvas)
+  
   let { initialDrawing } = canvasCreator();
-  //initialDrawing would draw the frame
+  
   initialDrawing();
 };
 
-//Canvas
+
 const canvasCreator = () => {
   let context = canvas.getContext("2d");
   context.beginPath();
   context.strokeStyle = "#000";
   context.lineWidth = 2;
 
-  //For drawing lines
+  
   const drawLine = (fromX, fromY, toX, toY) => {
     context.moveTo(fromX, fromY);
     context.lineTo(toX, toY);
@@ -518,24 +525,24 @@ const canvasCreator = () => {
     drawLine(70, 80, 90, 110);
   };
 
-  //initial frame
+  
   const initialDrawing = () => {
-    //clear canvas
+    
     context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-    //bottom line
+   
     drawLine(10, 130, 130, 130);
-    //left line
+   
     drawLine(10, 10, 10, 131);
-    //top line
+   
     drawLine(10, 10, 70, 10);
-    //small top line
+    
     drawLine(70, 10, 70, 20);
   };
 
   return { initialDrawing, head, body, leftArm, rightArm, leftLeg, rightLeg };
 };
 
-//draw the man
+
 const drawMan = (count) => {
   let { head, body, leftArm, rightArm, leftLeg, rightLeg } = canvasCreator();
   switch (count) {
@@ -562,6 +569,6 @@ const drawMan = (count) => {
   }
 };
 
-//New Game
+
 newGameButton.addEventListener("click", initializer);
 window.onload = initializer;
